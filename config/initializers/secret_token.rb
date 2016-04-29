923aaae2484cdc68e56ccecff7ebdfd2b38cda5c
Archana Raut <rautarchana9@gmail.com>
2016-04-29 09:53:47 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExpApp::Application.config.secret_key_base = 'fe201e447418e627ed6803ff47f1a6fa49cefd22f0a5dfe0d67c065fba6d20f28c45f27ef1d7d31e4f8effd0554bf6c95b8e84b17cd30939ac89e4454db91a4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExpApp::Application.config.session_store :cookie_store, key: '_exp-app_session'
